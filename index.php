13a7e0cf0a522ef48be12315299653895b5135fe
KurtSalonga <kasalonga23-0391@cca.edu.ph>
2025-02-19 18:52:27 +0800

    FORM SAMPLE
    HTML FORM FILE

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
+	<title>Javascript Form</title>
 </head>
 <body>
-<form action="/action_page.php">
+<form action="action_page.php method="POST">
   <fieldset>
     <legend>Personalia:</legend>
     <label for="fname">First name:</label>
@@ -17,7 +17,7 @@
     <input type="email" id="email" name="email"><br><br>
     <label for="birthday">Birthday:</label>
     <input type="date" id="birthday" name="birthday"><br><br>
-    <input type="submit" value="Submit">
+    <input type="submit" value="Submit" name="submit_record">
   </fieldset>
 </form>
 
